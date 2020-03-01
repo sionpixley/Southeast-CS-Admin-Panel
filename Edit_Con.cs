d8bc8e5b1018dded1720584aa027650cf572f40d
jsmith120 <50155444+jsmith120@users.noreply.github.com>
2020-02-29 23:16:28 -0600

    Add files via upload

---
+++ Edit_Con.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Admin_Control_Panel
+{
+    public partial class Edit_Con : Form
+    {
+        public Edit_Con()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form_Con con = new Form_Con();
+            con.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Edit_Event.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Admin_Control_Panel
+{
+    public partial class Edit_Event : Form
+    {
+        public Edit_Event()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f3 = new Form3();
+            f3.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form_Event eve = new Form_Event();
+            eve.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
