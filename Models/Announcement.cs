279663e26b6c672ee995368438019d2796107dfa
Sion Pixley <sion.pixley@wwinc.com>
2020-03-06 11:35:40 -0600

    Created Models/ and installed Json.Net.

---
+++ Models/Announcement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_Control_Panel.Models
+{
+    public class Announcement
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+        public DateTime authored_date { get; set; }
+        public string subject { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_Control_Panel.Models
+{
+    public class Event
+    {
+        public int id { get; set; }
+        public DateTime date { get; set; }
+        public string location { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string organization { get; set; }
+    }
+}
