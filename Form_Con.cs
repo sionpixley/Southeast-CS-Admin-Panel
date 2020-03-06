e7ae03b874d812ea35a6668ddfa0550e89747ecc
Sion Pixley <sion.pixley@wwinc.com>
2020-03-06 12:41:31 -0600

    Contacts can be created.

--- Form_Con.cs
+++ Form_Con.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Admin_Control_Panel.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,6 +34,51 @@ namespace Admin_Control_Panel
             Form2 f1 = new Form2();
             f1.ShowDialog();
             this.Close();
+        }
+
+        private async void Submit_C_bt_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string email = textBox4.Text;
+            string phone = textBox3.Text;
+            string office = textBox2.Text;
+
+            var obj = new Dictionary<string, string>
+            {
+                { "name", name },
+                { "email", email },
+                { "phone", phone },
+                { "office", office }
+            };
+            string json = JsonConvert.SerializeObject(obj);
+
+            var uri = new Uri(string.Format(ApiClient.uriBase + "add-contact/", string.Empty));
+            try
+            {
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await ApiClient.httpClient.PostAsync(uri, httpContent);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Contact created.");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+                    textBox4.Clear();
+                }
+                else if(response.StatusCode == System.Net.HttpStatusCode.Found)
+                {
+                    MessageBox.Show("Contact already exists.");
+                }
+                else
+                {
+                    MessageBox.Show("Error. Form_Con.cs Submit_C_bt_Click() try1");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error. Form_Con.cs Submit_C_bt_Click() catch1");
+            }
         }
     }
 }
