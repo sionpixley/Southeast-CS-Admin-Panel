0772aecb74b684b78d1ac8e6c2dda7cfdd2ba789
Sion Pixley <sion.pixley@wwinc.com>
2020-03-09 12:39:31 -0500

    Formatting fixes.

--- Article_Edit.cs
+++ Article_Edit.cs
@@ -2,14 +2,10 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Admin_Control_Panel
 {
--- Models/ApiClient.cs
+++ Models/ApiClient.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
+
 
 namespace Admin_Control_Panel.Models
 {
